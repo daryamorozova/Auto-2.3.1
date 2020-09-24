df63aefd6981bfb627dba9dc62fdcde71adc582b
Darya Morozova <fitosmd@yandex.ru>
2020-09-24 13:26:04 +0300

    Initial commit

---
+++ src/main/java/ru/netology/netology/DataGenerator.java
@@ -0,0 +1,20 @@
+package ru.netology.netology;
+
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {}
+
+    public static class Delivery {
+        private Delivery() {}
+
+//        public static DeliveryDataInfo generateByDelivery(String locale) {
+//            Faker faker = new Faker(new Locale("ru"));
+//            return new DeliveryDataInfo(faker.address().cityName(),
+//                    faker.name().fullName(),
+//                    faker.phoneNumber().cellPhone());
+//        }
+
+    }
+
+}
